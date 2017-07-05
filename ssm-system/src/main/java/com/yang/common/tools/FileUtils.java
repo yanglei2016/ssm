915eaa1ce1569958580944d6429fd09748962312
zhyanglei.love@163.com <zhyanglei.love@163.com>
2017-07-05 15:23:57 +0800

    提交文件、字符串处理工具类

---
+++ ssm-system/src/main/java/com/yang/common/tools/FileUtils.java
@@ -0,0 +1,9 @@
+package com.yang.common.tools;
+/**
+ * 
+ * @author yanglei
+ * 2017年7月5日 下午3:22:36
+ */
+public class FileUtils {
+
+}
---
+++ ssm-system/src/main/java/com/yang/common/tools/SSMStrUtils.java
@@ -0,0 +1,9 @@
+package com.yang.common.tools;
+/**
+ * 
+ * @author yanglei
+ * 2017年7月5日 下午3:23:17
+ */
+public class SSMStrUtils {
+
+}
