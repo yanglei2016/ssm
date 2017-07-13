f4d48c10c5a801f3452548e2adfd540d09a4783d
zhyanglei.love@163.com <zhyanglei.love@163.com>
2017-07-13 16:41:56 +0800

    调整配置

--- ssm-core/src/main/java/com/yang/common/tools/redis/RedisLock.java
+++ ssm-core/src/main/java/com/yang/common/tools/redis/RedisLock.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import redis.clients.jedis.ShardedJedis;
 
@@ -13,7 +12,6 @@ import redis.clients.jedis.ShardedJedis;
  * 
  * @author yanglei 2017年7月13日 下午3:14:40
  */
-@Component
 public class RedisLock {
 	
 	private final Logger logger = LoggerFactory.getLogger(getClass());
