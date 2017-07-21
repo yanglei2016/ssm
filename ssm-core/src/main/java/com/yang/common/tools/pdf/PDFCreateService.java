fc32e825cae82554b43dbfd377003b9a31a4a954
zhyanglei.love@163.com <zhyanglei.love@163.com>
2017-07-21 17:00:18 +0800

    pdf

--- ssm-core/src/main/java/com/yang/common/tools/pdf/PDFCreateService.java
+++ ssm-core/src/main/java/com/yang/common/tools/pdf/PDFCreateService.java
@@ -10,6 +10,7 @@ import org.apache.log4j.Logger;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 import com.yang.common.tools.pdf.entity.BaseNodeVo;
 import com.yang.common.tools.pdf.entity.ImageNodeVo;
 import com.yang.common.tools.pdf.entity.PNodeVo;
@@ -18,14 +19,17 @@ import com.yang.common.tools.pdf.entity.TableNodeVo;
 public class PDFCreateService implements PdfPageEvent {
 	private Logger logger = Logger.getLogger(PDFCreateService.class);
 
-	public BaseFont bf;
-
-	private PdfTemplate total;
 	
 	/**页眉 */  
     public String header = "页眉";  
-    /**文档字体大小，页脚页眉最好和文本大小一致 */  
+	/**文档字体大小，页脚页眉最好和文本大小一致 */  
     public int presentFontSize = 10;  
+	/**文档页面大小，最好前面传入，否则默认为A4纸张 */  
+    public Rectangle pageSize = PageSize.A4;
+    /**模板*/  
+	private PdfTemplate total;
+	/**基础字体对象*/  
+	public BaseFont bf;
     /**利用基础字体生成的字体对象，一般用于生成中文文字  */  
     public Font fontDetail = null;  
 
@@ -78,7 +82,7 @@ public class PDFCreateService implements PdfPageEvent {
 			document.open();
 			// headerTable = HeaderCreate.create(globalModel,
 			// document.getPageSize().getWidth());
-
+			
 			for (BaseNodeVo baseNodeVo : listNodes) {
 				try {
 					if (baseNodeVo instanceof PNodeVo) {
@@ -141,7 +145,7 @@ public class PDFCreateService implements PdfPageEvent {
 		try {
 			isNewPage = true;
 			bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
-			total = writer.getDirectContent().createTemplate(30, 10);
+			total = writer.getDirectContent().createTemplate(50, 50);// 共 页 的矩形的长宽高  
 		} catch (Exception e) {
 
 		}
@@ -163,112 +167,52 @@ public class PDFCreateService implements PdfPageEvent {
 	@Override
 	public void onCloseDocument(PdfWriter writer, Document arg1) {
 		// 7.最后一步了，就是关闭文档的时候，将模板替换成实际的 Y 值,至此，page x of y 制作完毕，完美兼容各种文档size。  
-//        total.beginText();  
-//        total.setFontAndSize(bf, 10);// 生成的模版的字体、颜色  
-//        total.setTextMatrix(0, 0);
-//        total.showText("" + (writer.getPageNumber()) + "");// 模版显示的内容  
-//        total.endText();  
-//        total.closePath();  
-        
-        ColumnText.showTextAligned(total,Element.ALIGN_LEFT,new Phrase(String.valueOf(writer.getPageNumber()-1)),2,2,0);
+        total.beginText();  
+        total.setFontAndSize(bf, presentFontSize);// 生成的模版的字体、颜色  
+        String foot2 = " " + (writer.getPageNumber()) + " 页";  
+        total.showText(foot2);// 模版显示的内容  
+        total.endText();  
+        total.closePath();  
 	}
 
 	@Override
 	public void onEndPage(PdfWriter writer, Document document) {
 		isNewPage = true;
 		
-		PdfPTable table = new PdfPTable(3);
-	     try{
-	     table.setWidths(new int[]{24,24,2});
-	     table.setTotalWidth(document.getPageSize().getWidth() - 72);
-	     table.setLockedWidth(true);
-	     table.getDefaultCell().setFixedHeight(20);
-	     table.getDefaultCell().setBorder(Rectangle.BOTTOM);
-
-	     table.addCell(new Paragraph(header, new Font(bf, 10, 0)));
-	     table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
-	     table.addCell("");
-	     PdfPCell cell = new PdfPCell(Image.getInstance(total));
-	     cell.setBorder(Rectangle.BOTTOM);
-	     table.addCell(cell);
-	     table.writeSelectedRows(0,-1,34,803,writer.getDirectContent());
-
-	     }
-	     catch(DocumentException de){
-	     throw new ExceptionConverter(de);
-	     }
-	     
-//		// 页数
-//		PdfPTable table = new PdfPTable(2);
-//		try {
-//			table.setWidths(new int[] { 30, 30 });
-//			table.setTotalWidth(document.getPageSize().getWidth() - 72);
-//			table.setLockedWidth(true);
-//			
-//			table.setWidthPercentage(100);// 设置表格宽度为%100
-////			table.setSplitLate(false);
-////			table.setSplitRows(true);
-//			
-//			table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
-//			table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
-//			table.getDefaultCell().setBorderWidth(1);
-//			table.getDefaultCell().setBorder(Rectangle.TOP);
-//			Font font = new Font(bf, 10, 0);
-//			Paragraph paragraph = new Paragraph(String.format("%d/", writer.getPageNumber()), font);
-//			table.addCell(paragraph);
-//
-//			PdfPCell cell = new PdfPCell(Image.getInstance(total));
-//			cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-//			cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-//			cell.setBorder(Rectangle.TOP);
-//			cell.setBorderColorTop(BaseColor.RED);
-//			cell.setBorderWidth(1);
-//			table.addCell(cell);
-//			table.writeSelectedRows(0, -1, 0, 30, writer.getDirectContent());
-//
-//		} catch (DocumentException e) {
-//			logger.error("创建页码异常", e);
-//		}
-		
-//		try {  
-//            if (bf == null) {  
-//                bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", false);  
-//            }  
-//            if (fontDetail == null) {  
-//                fontDetail = new Font(bf, presentFontSize, Font.NORMAL);// 数据体字体  
-//            }  
-//        } catch (DocumentException e) {  
-//            e.printStackTrace();  
-//        } catch (IOException e) {  
-//            e.printStackTrace();  
-//        }  
-//    
-//        // 1.写入页眉  
-//        ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_LEFT, new Phrase(header, fontDetail), document.left(), document.top() + 20, 0);  
-//           
-//        // 2.写入前半部分的 第 X页/共  
-//        int pageS = writer.getPageNumber();  
-//        String foot1 = "第 " + pageS + " 页 /共";  
-//        Phrase footer = new Phrase(foot1, fontDetail);  
-//    
-//        // 3.计算前半部分的foot1的长度，后面好定位最后一部分的'Y页'这俩字的x轴坐标，字体长度也要计算进去 = len  
-//        float len = bf.getWidthPoint(foot1, presentFontSize);  
-//    
-//        // 4.拿到当前的PdfContentByte  
-//        PdfContentByte cb = writer.getDirectContent();  
-//         
-//        //自己增加的  
-//        if(pageS==1){  
-//	        Phrase footerLeft = new Phrase("", fontDetail);  
-//	        ColumnText.showTextAligned(cb, Element.ALIGN_LEFT, footerLeft, document.left(), document.bottom() - 20, 0);  
-//        }  
-//          
-//        // 5.写入页脚1，x轴就是(右margin+左margin + right() -left()- len)/2.0F 再给偏移20F适合人类视觉感受，否则肉眼看上去就太偏左了 ,y轴就是底边界-20,否则就贴边重叠到数据体里了就不是页脚了；注意Y轴是从下往上累加的，最上方的Top值是大于Bottom好几百开外的。  
-//        ColumnText.showTextAligned(cb, Element.ALIGN_CENTER, footer, (document.rightMargin() + document.right() + document.leftMargin() - document.left() - len) / 2.0F + 20F, document.bottom() - 20, 0);  
-//    
-//        // 6.写入页脚2的模板（就是页脚的Y页这俩字）添加到文档中，计算模板的和Y轴,X=(右边界-左边界 - 前半部分的len值)/2.0F + len ， y 轴和之前的保持一致，底边界-20  
-//        cb.addTemplate(total, (document.rightMargin() + document.right() + document.leftMargin() - document.left()) / 2.0F + 20F, document.bottom() - 20); // 调节模版显示的位置  
-//		
+		try {  
+            if (bf == null) {  
+                bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", false);  
+            }  
+            if (fontDetail == null) {  
+                fontDetail = new Font(bf, presentFontSize, Font.NORMAL);// 数据体字体
+                //fontDetail.setColor(BaseColor.RED);
+            }  
+        } catch (DocumentException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+    
+        // 1.写入页眉  
+        ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_LEFT, new Phrase("页眉左边", fontDetail), document.left(), document.top() - 18, 0);
+        ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_RIGHT, new Phrase("页眉右边", fontDetail), document.right(), document.top() - 18, 0);
+        
+        // 2.写入前半部分的 第 X页/共  
+        int pageS = writer.getPageNumber();  
+        String foot1 = "第 " + pageS + " 页 /共";  
+        Phrase footer = new Phrase(foot1, fontDetail);  
+    
+        // 3.计算前半部分的foot1的长度，后面好定位最后一部分的'Y页'这俩字的x轴坐标，字体长度也要计算进去 = len  
+        float len = bf.getWidthPoint(foot1, presentFontSize);  
+    
+        // 4.拿到当前的PdfContentByte  
+        PdfContentByte cb = writer.getDirectContent();  
+    
+        // 5.写入页脚1，x轴就是(右margin+左margin + right() -left()- len)/2.0F 再给偏移20F适合人类视觉感受，否则肉眼看上去就太偏左了 ,y轴就是底边界-20,否则就贴边重叠到数据体里了就不是页脚了；注意Y轴是从下往上累加的，最上方的Top值是大于Bottom好几百开外的。  
+        ColumnText.showTextAligned(cb, Element.ALIGN_CENTER, footer, (document.rightMargin() + document.right() + document.leftMargin() - document.left() - len) / 2.0F + 20F, document.bottom() - 20, 0);  
+    
+        // 6.写入页脚2的模板（就是页脚的Y页这俩字）添加到文档中，计算模板的和Y轴,X=(右边界-左边界 - 前半部分的len值)/2.0F + len ， y 轴和之前的保持一致，底边界-20  
+        cb.addTemplate(total, (document.rightMargin() + document.right() + document.leftMargin() - document.left()) / 2.0F + 20F, document.bottom() - 20); // 调节模版显示的位置  
 	}
 
 	@Override
@@ -302,8 +246,13 @@ public class PDFCreateService implements PdfPageEvent {
 	}
 
 	@Override
-	public void onStartPage(PdfWriter arg0, Document arg1) {
-		// TODO Auto-generated method stub
-
+	public void onStartPage(PdfWriter writer, Document document) {
+		try {
+			Paragraph line = new Paragraph("");  
+			line.add(new Chunk(new LineSeparator(1, 100, BaseColor.RED, Element.ALIGN_CENTER, -2)));  
+			document.add(line);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
 	}
 }
